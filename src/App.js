76b63121f9468f6fbeb54bd140731153aea5a4f6
“Natalia <natalia.rudiger@gmail.com>
2022-03-08 17:06:23 -0300

    iteration 1 modifications

--- src/App.js
+++ src/App.js
@@ -7,43 +7,29 @@ import CreditCard from './CreditCard';
 import Rating from './Rating';
 import DriverCard  from './DriverCard';
 
-const idCard = [ 
-  {
-  lastName: 'Doe',
-  firstName: 'John',
-  gender: 'male',
-  height: 178,
-  birth: new Date("1992-07-14"),
-  picture: "https://randomuser.me/api/portraits/men/44.jpg"
-  },
-  {
-  lastName:'Delores',
-  firstName: 'Obrien',
-  gender:'female',
-  height: 172,
-  birth: new Date("1988-05-11"),
-  picture: "https://randomuser.me/api/portraits/women/44.jpg"
-  }
-]
-
 
 function App() {
   return (
     <div>
       <div>
       <h1>IdCard</h1>
-        {idCard.map((currentID) => {
-         return (
-            <IdCard
-            lastName = {currentID.lastName}
-            firstName = {currentID.firstName}
-            gender = {currentID.gender}
-            height = {currentID.height/100}
-            birth = {currentID.birth.toDateString()}
-            picture = {currentID.picture}
-            />
-          )
-        })}
+      <IdCard
+        lastName = 'Doe'
+        firstName = 'John'
+        gender = 'male'
+        height = {178}
+        birth = {new Date("1992-07-14")}
+        picture = "https://randomuser.me/api/portraits/men/44.jpg"
+      />
+
+      <IdCard
+        lastName = 'Delores '
+        firstName = 'Obrien'
+        gender = 'female'
+        height = {172}
+        birth = {new Date("1988-05-11")}
+        picture = "https://randomuser.me/api/portraits/women/44.jpg"
+      />
       </div>
       <div>
         <h1>Greetings</h1>
--- src/IdCard.js
+++ src/IdCard.js
@@ -1,21 +1,21 @@
 import './index.css'
 
-function IdCard({ lastName, firstName, gender, height, birth, picture}) {
+function IdCard(props) {
     return (
        <div className = 'card-layout'>
             <div className = 'picture-layout'>
-                <img src = {picture} alt = "idCard"/>
+                <img src = {props.picture} alt = 'ID'/>
             </div>
             <div className='information-layout'>
-                <b>First name:</b> {firstName}
+                <b>First name:</b> {props.firstName}
                 <br/>
-                <b>Last name:</b> {lastName}
+                <b>Last name:</b> {props.lastName}
                 <br/>
-                <b>Gender:</b>{gender}
+                <b>Gender:</b> {props.gender}
                 <br/>
-                <b>Height:</b> {height}m
+                <b>Height:</b> {props.height/100}m
                 <br/>
-                <b>Birth:</b> {birth}
+                <b>Birth:</b> {props.birth.toDateString()}
             </div>
     </div>
     )
